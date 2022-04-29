83809e5ff5f9c277365418c0b6238dd1551b0980
deepakaneesh7 <104065134+deepakaneesh7@users.noreply.github.com>
2022-04-29 15:01:46 +0530

    Update shadowdomtesting.spec.js

--- shadowdomtesting.spec.js
+++ shadowdomtesting.spec.js
@@ -9,20 +9,20 @@ describe('Shadow Dom', async() => {
     await loginButton.click()  
   });
 
-  it('Click on Shadow Element', async () => {
+ it('Click on Shadow Element', async () => {
     const connect= await $('//h4[contains(text(),"test collab")]')
     await connect.waitForDisplayed({timeout : 30000})
     await connect.click()
-    browser.pause(5000)
-    const contactPerson= await $('//ul[@class="css-1019tpl-MemberItemList"]//li[2]')
+    await browser.pause(2000)
+    const contactPerson= await $('//ul[@class="css-1019tpl-MemberItemList"]//li[2]//i')
     await contactPerson.moveTo()
-    const nameBanner= await $('//div[@id="6daaaf79-2b77-4f40-b129-fe4eaee1f084"]//h4[contains(text(),"John")]')
+    const nameBanner= await $('//div[@id="6daaaf79-2b77-4f40-b129-fe4eaee1f084"]//h4')
     await nameBanner.waitForDisplayed({timeout : 5000})
     await nameBanner.click()
-    browser.pause(5000)
-    const shadowElement= await $('#contact-card').shadow$('#start_chat')
+    await browser.pause(3000)
+    const shadowElement= await $('#contact-card').shadow$('span[data-option-id="start_chat"]')
     await shadowElement.waitForDisplayed()
     await shadowElement.click()
-    browser.pause(10000)
+    await browser.pause(3000)
   });
 });
